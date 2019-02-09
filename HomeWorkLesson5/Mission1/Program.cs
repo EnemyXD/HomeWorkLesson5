20bcf6116fe3f259219153ff8eeefed042aa7388
enemy <nkalmykov937@gmail.com>
2019-02-09 11:57:05 +0300

    Создан пустой проект

---
+++ HomeWorkLesson5/Mission1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Mission1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+        }
+    }
+}
