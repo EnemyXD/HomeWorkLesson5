ee1ddc2381704fe9ff316e03f23702f6e1486c72
enemy <nkalmykov937@gmail.com>
2019-02-09 15:46:49 +0300

    Выполнил задание номер 1

--- HomeWorkLesson5/Mission1/Program.cs
+++ HomeWorkLesson5/Mission1/Program.cs
@@ -11,8 +11,36 @@ namespace Mission1
     {
         static void Main(string[] args)
         {
-            
 
+
+            //while (true)
+            //{
+
+            //    string login = Console.ReadLine();
+
+            //    bool access = NotRegular.Not(login);
+            //    if (access == true)
+            //    {
+            //        Console.WriteLine("Логин корректный");
+            //    }
+            //    else Console.WriteLine("Логин не корректный");
+
+            //}
+
+            while (true)
+            {
+
+
+                string login = Console.ReadLine();
+                bool reg = Regex.IsMatch(login, @"^[\D]{1,}[A-Za-z0-9]{2,9}");
+                if (reg == true)
+                {
+                    Console.WriteLine("Логин корректный");
+                }
+                else Console.WriteLine("Логин не корректный");
+
+            }
+            Console.ReadKey();
 
         }
     }
