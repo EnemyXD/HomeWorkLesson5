7ccd6d122e3ddf902ff50fe4d3424c1c61b9c1c7
enemy <nkalmykov937@gmail.com>
2019-02-09 15:51:10 +0300

    Исправил первое задание

--- HomeWorkLesson5/Mission1/Program.cs
+++ HomeWorkLesson5/Mission1/Program.cs
@@ -5,6 +5,13 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
+
+//Калмыков
+//Создать программу, которая будет проверять корректность ввода логина.
+//Корректным логином будет строка от 2 до 10 символов, содержащая только буквы латинского алфавита или цифры, при этом цифра не может быть первой:
+//а) без использования регулярных выражений;
+//б) с использованием регулярных выражений.
+
 namespace Mission1
 {
     class Program
@@ -13,33 +20,33 @@ namespace Mission1
         {
 
 
-            //while (true)
-            //{
-
-            //    string login = Console.ReadLine();
-
-            //    bool access = NotRegular.Not(login);
-            //    if (access == true)
-            //    {
-            //        Console.WriteLine("Логин корректный");
-            //    }
-            //    else Console.WriteLine("Логин не корректный");
-
-            //}
-
-            while (true)
-            {
-
+            //без использования регулярных выражений
+            
 
                 string login = Console.ReadLine();
-                bool reg = Regex.IsMatch(login, @"^[\D]{1,}[A-Za-z0-9]{2,9}");
+
+                bool access = NotRegular.Not(login);
+                if (access == true)
+                {
+                    Console.WriteLine("Логин корректный");
+                }
+                else Console.WriteLine("Логин не корректный");
+
+            
+
+
+            //с использованием регулярных выржений
+
+
+             
+                bool reg = Regex.IsMatch(login, @"^[\D]{1,}[A-Za-zА-Яа-я0-9]{2,9}");
                 if (reg == true)
                 {
                     Console.WriteLine("Логин корректный");
                 }
                 else Console.WriteLine("Логин не корректный");
 
-            }
+            
             Console.ReadKey();
 
         }
